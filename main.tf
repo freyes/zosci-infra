081ff57864dad20ec7f9d8d7ef3845ed52cb6029
Jadon Naas <jadon.naas@canonical.com>
2023-08-31 15:51:36 -0400

    Added Zookeeper StatefulSet

--- main.tf
+++ main.tf
@@ -100,12 +100,12 @@ resource "kubernetes_stateful_set" "mysql" {
                     resources {
                         limits = {
                             cpu = "1000m"
-                            memory = "2000Mi"
+                            memory = "500Mi"
                         }
 
                         requests = {
                             cpu = "1000m"
-                            memory = "2000Mi"
+                            memory = "500Mi"
                         }
                     }
 
@@ -136,6 +136,183 @@ resource "kubernetes_stateful_set" "mysql" {
                 access_modes = ["ReadWriteOnce"]
                 storage_class_name = "microk8s-hostpath"
 
+                resources {
+                    requests = {
+                        storage = "10Gi"
+                    }
+                }
+            }
+        }
+    }
+}
+
+resource "kubernetes_service" "zookeeper_hs" {
+    metadata {
+        name = "zk-hs"
+        labels = {
+            k8s-app = "zookeeper"
+        }
+    }
+
+    spec {
+        selector = {
+            k8s-app = "zookeeper"
+        }
+        port {
+            port = 2888
+            name = "server"
+        }
+        port {
+            port = 3888
+            name = "leader-election"
+        }
+        cluster_ip = "None"
+    }
+}
+
+resource "kubernetes_service" "zookeeper_cs" {
+    metadata {
+        name = "zk-cs"
+        labels = {
+            k8s-app = "zookeeper"
+        }
+    }
+
+    spec {
+        selector = {
+            k8s-app = "zookeeper"
+        }
+        port {
+            port = 2181
+            name = "client"
+        }
+    }
+}
+
+resource "kubernetes_pod_disruption_budget_v1" "zookeeper_pdb" {
+    metadata {
+        name = "zk-pdb"
+    }
+    
+    spec {
+        selector {
+            match_labels = {
+                k8s-app = "zookeeper"
+            }
+        }
+        max_unavailable = 1
+    }
+}
+
+resource "kubernetes_stateful_set" "zookeeper" {
+    metadata {
+        labels = {
+            k8s-app                             ="zookeeper"
+            "kubernetes.io/cluster-service"     = "true"
+            "addonmanager.kubernetes.io/mode"   = "Reconcile"
+            version                             = "3"
+        }
+        name = "zookeeper"
+        namespace = kubernetes_namespace.zuul.metadata[0].name
+    }
+
+    spec {
+        pod_management_policy = "OrderedReady"
+        replicas = 1
+        revision_history_limit = 5
+
+        selector {
+            match_labels = {
+              k8s-app = "zookeeper"
+            }
+        }
+
+        service_name = "zk-hs"
+        update_strategy {
+            type = "RollingUpdate"
+        }
+
+        template {
+            metadata {
+                labels = {
+                    k8s-app = "zookeeper"
+                }
+                annotations = {}
+            }
+
+            spec {
+                affinity {
+                    pod_anti_affinity {
+                        required_during_scheduling_ignored_during_execution {
+                            label_selector {
+                                match_expressions {
+                                    key = "k8s-app"
+                                    operator = "In"
+                                    values = ["zookeeper"]
+                                }
+                            }
+                            topology_key = "kubernetes.io/hostname"
+                        }
+                    }
+                }
+                container {
+                    name = "kubernetes-zookeeper"
+                    image_pull_policy = "Always"
+                    image = "zookeeper:3.9"
+                    resources {
+                        requests = {
+                            memory  = "1Gi"
+                            cpu     = "0.5"
+                        }
+                    }
+                    port {
+                        container_port = 2181
+                        name = "client"
+                    }
+
+                    port {
+                        container_port = 2888
+                        name = "server"
+                    }
+
+                    port {
+                        container_port = 3888
+                        name = "leader-election"
+                    }
+
+                    env {
+                        name = "ZOO_INIT_LIMIT"
+                        value = "10"
+                    }
+
+                    env {
+                        name = "ZOO_SYNC_LIMIT"
+                        value = "5"
+                    }
+
+                    env {
+                        name = "ZOO_CFG_EXTRA"
+                        value = "heap=512M logLevel=INFO"
+                    }
+
+                    volume_mount {
+                        name        = "datadir"
+                        mount_path  = "/var/lib/zookeeper"
+                    }
+                }
+                security_context {
+                    run_as_user = 1000
+                    fs_group    = 1000
+                }
+            }
+        }
+        volume_claim_template {
+            metadata {
+                name = "datadir"
+            }
+
+            spec {
+                access_modes = ["ReadWriteOnce"]
                 resources {
                     requests = {
                         storage = "10Gi"
